67e57f9b0adfbac760de291dd02ef5695c21bae1
Omar Mahdy <72965518+mahdtomar@users.noreply.github.com>
2023-03-28 20:02:38 +0200

    0

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 const root = document.querySelector(":root");
 const caption = document.querySelector(".before-after");
+const number = document.querySelector(".counter");
+const prices = document.querySelectorAll(".price");
+console.log(prices);
+function increment(count, target, targervalue) {
+  setInterval(() => {
+    if (count < targervalue) {
+      count++;
+      target.innerText = count;
+    }
+  }, 5);
+}
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    let count = 0;
+    let target = entry.target;
+    let targetNumber = parseInt(entry.target.getAttribute("data-value"));
+    console.log(parseInt(targetNumber));
+    entry.target.textContent = 0;
+    increment(count, target, targetNumber);
+  });
+  console.log(entries);
+});
+observer.observe(number);
 let rootStyles = getComputedStyle(root);
 document.querySelector("#slider").addEventListener("input", (e) => {
   root.style.setProperty("--position", `${e.target.value}%`);
@@ -9,4 +32,15 @@ document.querySelector("#slider").addEventListener("mouseenter", (e) => {
 });
 document.querySelector("#slider").addEventListener("mouseleave", (e) => {
   caption.classList.remove("hidden");
+});
+$(".menu").click(function () {
+  $(".nav_ul").addClass("visible");
+});
+$(".close-icon").click(function () {
+  $(".nav_ul").removeClass("visible");
+});
+
+$(".nav_ul li a").click(function (e) {
+  $(".nav_ul li a").removeClass("active");
+  e.target.classList.add("active");
 });
